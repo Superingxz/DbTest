e1fc5162d3c318c928d3f5c9cadf51aebb8d0ddc
moyaozhi <moyaozhi@aliyun.com>
2021-03-05 19:45:49 +0800

    Initial commit

---
+++ module1/src/main/java/com/moligy/module1/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.moligy.module1
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.moligy.module1.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var mBinding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        mBinding.lifecycleOwner = this
+    }
+}
